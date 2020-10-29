9fd61ba8e822b69c3b0ec76d6415ab04376c24cd
Tokamak-307 <52354748+Tokamak-307@users.noreply.github.com>
2020-10-29 19:22:34 +0800

    Add door but not complete

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public enum Direction { up, down, left, right };
+    public Direction direction;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.gameObject.CompareTag("Player")){
+            Room room = gameObject.GetComponentInParent(typeof(Room)) as Room;
+            if (room != null){
+                room.PlayerEnter();
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        
+        if (other.gameObject.CompareTag("Player")){
+            Room room = gameObject.GetComponentInParent(typeof(Room)) as Room;
+            if (room != null){
+                room.PlayerExit();
+            }
+        }
+    }
+}
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public GameObject doorLeft, doorRight, doorUp, doorDown; /**< 保存门的prefab对象*/
-    
-    public bool flagLeft, flagRight, flagUp, flagDown; /**< 记录上下左右是否有房间，以此判断是否需要生成门*/
+    [Header("房间属性")]
+    public float width = 10;
+    public float height = 10;
 
+    [Header("关联预制体")]
+    public GameObject doorUp; /**< 保存门的prefab对象*/
+    public GameObject doorDown;
+    public GameObject doorLeft;
+    public GameObject doorRight;
+
+    public bool flagUp { set; private get; }
+    public bool flagDown { set; private get; }
+    public bool flagLeft { set; private get; } /**< 记录上下左右是否有房间，以此判断是否需要生成门*/
+    public bool flagRight { set; private get; }
     
     void Start()
     {
@@ -17,9 +27,20 @@ public class Room : MonoBehaviour
         doorDown.SetActive(flagDown);
     }
 
-    
-    void Update()
-    {
-        
+    public void PlayerEnter() {
+        CompositeCollider2D collider = GetComponentInChildren(typeof(CompositeCollider2D)) as CompositeCollider2D;
+        if (collider != null) {
+            collider.enabled = false;
+            Debug.Log("Disable collider: " + collider);
+        }
     }
+
+    public void PlayerExit() {
+        CompositeCollider2D collider = GetComponentInChildren(typeof(CompositeCollider2D)) as CompositeCollider2D;
+        if (collider != null) {
+            collider.enabled = true;
+            Debug.Log("Enable collider: " + collider);
+        }
+    }
+
 }
