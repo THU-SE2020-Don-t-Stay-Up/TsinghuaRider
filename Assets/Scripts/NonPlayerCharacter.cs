e7bcdfb0bfaeb48520eb3a6f59cc1d75eea30fba
DingSiqin <48652981+Iamtheboneofmysword@users.noreply.github.com>
2020-11-02 17:03:33 +0800

    NPC和机关交互的脚本初版

---
+++ Assets/Scripts/NonPlayerCharacter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NonPlayerCharacter : MonoBehaviour
+{
+    // interaction
+    public float displayTime = 4.0f;
+    public GameObject dialogBox;
+    float timerDisplay;
+
+    // Pictorial Book related
+    // private sting npcName = transform.name;
+
+    private void Start(){
+        dialogBox.SetActive(false);
+        timerDisplay = -1.0f;
+    }
+
+    private void Update(){
+        if (timerDisplay >= 0){
+            timerDisplay -= Time.deltaTime;
+            if (timerDisplay < 0){
+                dialogBox.SetActive(false);
+            }
+        }
+    }
+
+    public void DisplayDialog()
+    {
+        timerDisplay = displayTime;
+        dialogBox.SetActive(true);
+    }
+
+    // pictorial book
+    //private void CheckPictorialBook(){
+    //
+    //  }
+}
+
