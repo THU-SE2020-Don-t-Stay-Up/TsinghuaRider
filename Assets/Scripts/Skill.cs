f4f3ae1b4c128dbdef8ca1be169d0a4c0f08923f
guhaiqiao <1019931868@qq.com>
2020-11-03 21:05:15 +0800

    实现怪物agent（简单攻击）以及生成

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -8,21 +8,27 @@ using UnityEngine.Analytics;
 
 abstract public class Skill
 {
-    public abstract void Perform(GameObject o);
+    public abstract void Perform(int Amount, GameObject target);
 }
 
 
 public class AttackSkill : Skill
 {
-    public override void Perform(GameObject o)
+    public override void Perform(int Amount, GameObject targetObjet)
     {
-        Transform position = o.transform;
+        Character_Robot target = targetObjet.GetComponent<Character_Robot>();
+        if (target == null)
+        {
+            // 异常处理
+        }
+        target.ChangeHealth(-Amount);
+        //Vector2 position = new Vector2(o.transform.position.x, o.transform.position.y);
     }
 }
 
 public class SplitSkill : Skill
 {
-    public override void Perform(GameObject o)
+    public override void Perform(int Anount, GameObject o)
     {
         Transform position = o.transform;
     }
