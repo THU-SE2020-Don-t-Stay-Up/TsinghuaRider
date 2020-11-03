bb32f94cd73bc8267243c4c81a23477fcfca139a
DingSiqin <48652981+Iamtheboneofmysword@users.noreply.github.com>
2020-11-03 21:01:12 +0800

    整理了aim和attack的逻辑，attack实现并入人物控制脚本，PlayerAim专门控制瞄准

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -14,7 +14,26 @@ public class PlayerAim : MonoBehaviour
 
     private void Update() {
         HandleAiming();
-        HandleActing();
+    }
+
+    public Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
     }
 
     private void HandleAiming() {
@@ -22,38 +41,6 @@ public class PlayerAim : MonoBehaviour
         Vector3 aimDir = (mousePosition - transform.position).normalized;
         float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
         aimTransform.eulerAngles = new Vector3(0,0,angle);
-    }
-
-    private void HandleActing() {
-        if (Input.GetMouseButtonDown(0)) {
-            Vector3 mousePosition = GetMouseWorldPosition();
-            mahouShoujo.GetComponent<Character_MahouShoujo>().MissleAttack(mousePosition);
-        }
-        if (Input.GetMouseButtonDown(1))
-        {
-            Vector3 mousePosition = GetMouseWorldPosition();
-            mahouShoujo.GetComponent<Character_MahouShoujo>().MeleeAttack(mousePosition);
-        }
-    }
-
-    private Vector3 GetMouseWorldPosition()
-    {
-        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
-        vec.z = 0f;
-        return vec;
-    }
-    private static Vector3 GetMouseWorldPositionWithZ()
-    {
-        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
-    }
-    private static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
-    {
-        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
-    }
-    private static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
-    {
-        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
-        return worldPosition;
     }
 
 }
