28d105aa7efd9547941f9b0d9f67f0a9dbd50aee
DingSiqin <48652981+Iamtheboneofmysword@users.noreply.github.com>
2020-11-03 19:39:25 +0800

    更新了脚本中攻击部分

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -5,27 +5,11 @@ using UnityEngine;
 public class PlayerAim : MonoBehaviour
 {
     private Transform aimTransform;
-    private GameObject Character_MahouShoujo;
+    private GameObject mahouShoujo;
 
     private void Awake() {
         aimTransform = transform.Find("Aim");
-        Character_MahouShoujo = GameObject.Find("MahouShoujo");
-    }
-
-    private Vector3 GetMouseWorldPosition() {
-        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
-        vec.z = 0f;
-        return vec;
-    }
-    private static Vector3 GetMouseWorldPositionWithZ(){
-        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
-    }
-    private static Vector3 GetMouseWorldPositionWithZ(Camera  worldCamera){
-        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
-    }
-    private static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera){
-        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
-        return worldPosition;
+        mahouShoujo = GameObject.Find("MahouShoujo");
     }
 
     private void Update() {
@@ -38,17 +22,38 @@ public class PlayerAim : MonoBehaviour
         Vector3 aimDir = (mousePosition - transform.position).normalized;
         float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
         aimTransform.eulerAngles = new Vector3(0,0,angle);
-      
     }
 
     private void HandleActing() {
-        if (Input.GetMouseButtonDown(0)){
+        if (Input.GetMouseButtonDown(0)) {
             Vector3 mousePosition = GetMouseWorldPosition();
-            //Vector3 aimDir = (mousePosition - transform.position).normalized;
-            //Vector2 direction = new Vector2(0,0);
-            //direction.Set(aimDir.x, aimDir.y);
-            //rubyController.GetComponent<RubyController>().Launch(direction);
+            mahouShoujo.GetComponent<Character_MahouShoujo>().MissleAttack(mousePosition);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 mousePosition = GetMouseWorldPosition();
+            mahouShoujo.GetComponent<Character_MahouShoujo>().MeleeAttack(mousePosition);
         }
     }
-    
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+    private static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+    private static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    private static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+
 }
