30476fe15cc5868953f82a9dcc0cc3a233a05bdc
guhaiqiao <1019931868@qq.com>
2020-11-06 17:07:15 +0800

    实体代码重构

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,39 +1,29 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Dynamic;
 using System.IO;
-using System.Reflection;
-using UnityEngine;
 
-
-
-public class Monster
-{   
-    public string Name { get; set; }
-    public float MoveSpeed { get; set; }
-    public int MaxHealth { get; set; }
-    public int CurrentHealth { get; set; }
-    public int Defense { get; set; }
+/// <summary>
+/// 怪物属性类，继承自LivingBase，存储角色所有基本属性
+/// </summary>
+public class Monster : LivingBase
+{
     /// <summary>
-    /// 韧性，减少控制时长
+    /// 视野角度
     /// </summary>
-    public float Tenacity { get; }
-    public float AttackSpeed { get; set; }
-
-    public int AttackAmount { get; set; }
     public float ViewAngle { get; set; }
+    /// <summary>
+    /// 视野半径，玩家进入视野半径以内时追击玩家
+    /// </summary>
     public float ViewRadius { get; set; }
-    public float AttackRadius { get; set; }
-    public string TexturePath { get; }
+    /// <summary>
+    /// 击杀奖励
+    /// </summary>
     public int Reward { get; set; }
+    /// <summary>
+    /// 难度
+    /// </summary>
     public int Difficulty { get; set; }
-    [JsonConverter(typeof(SkillsJsonConverter))]
-    public List<Skill> Skills { get; set; }
-    public Status Status { get; set; }
+
 
     /// <summary>
     /// 加载怪物配置文件，游戏初始化时调用
@@ -53,34 +43,6 @@ public class Monster
         {
             JsonSerializer.CreateDefault().Serialize(new JsonTextWriter(sw), monsters);
         }
-    }
-}
-
-class SkillsJsonConverter : JsonConverter<List<Skill>>
-{
-    public override List<Skill> ReadJson(JsonReader reader, Type objectType, List<Skill> existingValue, bool hasExistingValue, JsonSerializer serializer)
-    {
-        if (existingValue == null) existingValue = new List<Skill>();
-        JArray arr = JArray.Load(reader);
-        foreach (var s in arr)
-        {
-            existingValue.Add((Skill)Activator.CreateInstance(typeof(Skill).Assembly.GetType(s.ToString() + "Skill")));
-        }
-        return existingValue;
-    }
-
-    public override void WriteJson(JsonWriter writer, List<Skill> value, JsonSerializer serializer)
-    {
-        writer.WriteStartArray();
-        if (value != null)
-        {
-            foreach (var s in value)
-            {
-                string name = s.GetType().Name;
-                writer.WriteValue(name.Substring(0, name.Length - 5));
-            }
-        }
-        writer.WriteEndArray();
     }
 }
 
