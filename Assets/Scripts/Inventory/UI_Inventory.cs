8854836d72eb58ed5171d3164c692120a39c3313
DingSiqin <48652981+Iamtheboneofmysword@users.noreply.github.com>
2020-11-09 09:30:51 +0800

    道具栏的UI（暂时有大问题，每一次重启unity都得手动拖一下脚本到UI组件，否则不能实例化）

---
+++ Assets/Scripts/Inventory/UI_Inventory.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Inventory : MonoBehaviour
+{
+    private Inventory inventory;
+    private Transform itemSlotContainer;
+    private Transform itemSlotTemplate;
+
+    private void Awake()
+    {
+        itemSlotContainer = transform.Find("itemSlotContainer");
+        itemSlotTemplate = itemSlotContainer.Find("itemSlotTemplate");
+    }
+
+    public  void SetInventory(Inventory inventory)
+    {
+        this.inventory = inventory;
+
+        inventory.OnItemListChanged += Inventory_OnItemListChanged;
+        RefreshInventoryItems();
+    }
+
+    private void Inventory_OnItemListChanged(object sender, System.EventArgs e)
+    {
+        RefreshInventoryItems();
+    }
+
+    private void RefreshInventoryItems()
+    {
+        foreach (Transform child in itemSlotContainer)
+        {
+            if (child == itemSlotTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        int x = 0;
+        int y = 0;
+        float itemSlotCellSize = 50.0f;
+        foreach (Item item in inventory.GetItemList())
+        {
+            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+            itemSlotRectTransform.gameObject.SetActive(true);
+            itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
+            Image image = itemSlotRectTransform.Find("image").GetComponent<Image>();
+            image.sprite = item.GetSprite();
+
+            x++;
+            if (x > 4)
+            {
+                x = 0;
+                y --;
+            }
+        }
+    }
+}
