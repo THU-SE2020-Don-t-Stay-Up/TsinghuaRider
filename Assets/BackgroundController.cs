7978dcdc3673296f63163779142cd9dab38d1f22
Tokamak-307 <52354748+Tokamak-307@users.noreply.github.com>
2020-12-13 16:49:47 +0800

    增加背景跟随摄像机移动效果

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public GameObject mainCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+        else
+        {
+            transform.position = mainCamera.transform.position * 0.6f;
+        }
+    }
+}
