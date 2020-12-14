6a852d1cd6172f567a5f389db37a71b696ecd283
guhaiqiao <1019931868@qq.com>
2020-12-14 09:40:01 +0800

    修复了刀光碰到怪物消失的bug

--- Assets/Scripts/SwordLight.cs
+++ Assets/Scripts/SwordLight.cs
@@ -2,6 +2,18 @@
 
 class SwordLight : Bullet
 {
+    public override void InteractWith(GameObject gameObject)
+    {
+        LivingBaseAgent agent = gameObject.GetComponent<LivingBaseAgent>();
+        if (agent != null)
+        {
+            if (!gameObject.CompareTag(UserTag))
+            {
+                agent.ChangeHealth(-Damage);
+                ExtraEffect?.Invoke(agent);
+            }
+        }
+    }
     public override void OnTriggerEnter2D(Collider2D collision)
     {
     }
