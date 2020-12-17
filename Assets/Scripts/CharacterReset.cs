f910c9b65d0b6bfe67f022be7504bce6bd8abbf8
Ding Siqin <844105091@163.com>
2020-12-17 11:43:26 +0800

    进入主世界后正确加载人物信息

---
+++ Assets/Scripts/CharacterReset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterReset : MonoBehaviour
+{
+    CharacterLoader characterLoder;
+    AcrossSceneController acrossSceneController;
+
+    private void Awake()
+    {
+        characterLoder = GameObject.Find("CharacterLoader").GetComponent<CharacterLoader>();
+        acrossSceneController = GameObject.Find("Across Scene Controller").GetComponent<AcrossSceneController>();
+        acrossSceneController.ui.SetActive(true);
+        characterLoder.player.GetComponent<CharacterAgent>().SetPortrait(acrossSceneController);
+
+        characterLoder.player.GetComponent<CharacterAgent>().Awake();
+
+
+    }
+
+}
