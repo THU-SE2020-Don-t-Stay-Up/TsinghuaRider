1e4a0169798b2aac413fb01e3c8d4570e52db074
Ding Siqin <844105091@163.com>
2020-12-18 11:28:43 +0800

    加入死亡后提示

---
+++ Assets/Scripts/UI/UIDealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDealth : MonoBehaviour
+{
+    public static UIDealth instance;
+    Transform deathText;
+
+    private void Start()
+    {
+        deathText = transform.Find("DeathText");
+        instance = this;
+        deathText.gameObject.SetActive(false);
+    }
+
+    public void DeathInfo()
+    {
+        deathText.gameObject.SetActive(true);
+    }
+    
+}
