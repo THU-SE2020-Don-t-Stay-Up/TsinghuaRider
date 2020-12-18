2895d68d46780d3a0af089e6c8dfd16af2f8eb58
Ding Siqin <844105091@163.com>
2020-12-18 09:14:24 +0800

    TestScene完善

---
+++ Assets/Scripts/LocalCharacterSet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalCharacterSet : MonoBehaviour
+{
+    GameObject mahou;
+    private void Awake()
+    {
+        UISelectCharacter.characterIndex = 0;
+        mahou = GameObject.Find("MahouPrefab");
+        CharacterAgent mahouAgent = mahou.GetComponent<CharacterAgent>();
+        mahouAgent.enabled = true;
+        mahouAgent.TestAwake();
+    }
+}
